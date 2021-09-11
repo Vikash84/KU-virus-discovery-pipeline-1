9b6e5d128a2b7c01e254760f0629f1bf36d4b11c
KijinKim <skkujin@gmail.com>
2021-09-11 16:57:15 +0900

    script to match taxon added

---
+++ scripts/match_taxonomy_to_blast.R
@@ -0,0 +1,25 @@
+args = commandArgs(trailingOnly=TRUE)
+
+# test if there is at least one argument: if not, return an error
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+} else if (length(args)==1) {
+  require(data.table)
+  require(dplyr)
+  require(taxonomizr)
+  data <- as.data.frame(fread(args[1]))
+}
+
+if (nrow(data)==0) {
+  quit()
+} else{
+  uniq_taxid <- unique(data$"TAXID")
+}
+
+all_taxons <- as.data.frame(getTaxonomy(uniq_taxid, args[2]))
+all_taxons <- tibble::rownames_to_column(all_taxons, "TAXID")
+all_taxons$TAXID <- as.integer(all_taxons$TAXID)
+
+taxon_mapped_data <- inner_join(data, all_taxon, by = "TAXID")
+
+write.table(taxon_mapped_data, file = args[1], row.names=FALSE, col.names = TRUE, sep="\t", quote = FALSE)
